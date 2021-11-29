bb540176f7067a4d57feade3c8db7af3c21bcdbd
Makrand <makrandshingare@gmail.com>
2021-11-29 14:12:14 +0530

    Check the lines are equal or not

--- src/LineComparison.java
+++ src/LineComparison.java
@@ -3,20 +3,24 @@ import java.util.Scanner;
 public class LineComparison {
 static int x1,y1,x2,y2;
     public static void main(String[] args) {
+        double lenght1=getInput();
+        double length2=getInput();
+        if(lenght1==length2){
+            System.out.println("lines are equals");
+        }
+        else System.out.println("Lines are not equal");
 
-        getInput();
-        calculatedLength();
     }
 
-    private static void calculatedLength() {
+    private static double calculatedLength() {
         double x=Math.pow((x2-x1), 2);
         double y=Math.pow((y2-y1), 2);
         double length = Math.floor(Math.sqrt(x+y));
-        System.out.println("Lenth of line : "+length);
+        return length;
     }
 
 
-    private static void getInput() {
+    private static double getInput() {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter Line Coordinate for first point");
         x1=sc.nextInt();
@@ -24,6 +28,7 @@ static int x1,y1,x2,y2;
         System.out.println("Enter Line Coordinate for Second point");
         x2=sc.nextInt();
         y2=sc.nextInt();
-
+        calculatedLength();
+        return calculatedLength();
     }
 }
